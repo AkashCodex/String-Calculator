3a990dd3b2ca138decd411a22dee05dd38ddaacb
AkashCodex <go4akasharora818@gmail.com>
2021-05-25 11:17:26 +0530

    Task-1 added add method

---
+++ StringCalculator.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class StringCalculator
+{
+    private readonly List<string> _defaultDelimiters = new List<string> { ",", "\n" };
+    private const int StartIndexOfNumbersWithCustomDelimiter = 3;
+    private const int StartIndexOfCustomDelimiter = 2;
+    private const int MaxNumberLimit = 1000;
+    private const string CustomDelimiterIdentifier = "//";
+
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers)) return 0;
+
+        if (numbers.StartsWith(CustomDelimiterIdentifier))
+        {
+            numbers = GetNumbersExcludingCustomDelimiter(numbers);
+        }
+
+        var sumOfNumbers = GetSumOfNumbers(numbers);
+
+        return sumOfNumbers;
+    }
+
+    private int GetSumOfNumbers(string numbers)
+    {
+        var convertedNumbers = numbers.Split(_defaultDelimiters.ToArray(), StringSplitOptions.None).Select(int.Parse).ToList();
+
+        ValidateNumbersArePositive(convertedNumbers);
+
+        var sumOfNumbers = convertedNumbers.Where(x => x <= MaxNumberLimit).Sum();
+        return sumOfNumbers;
+    }
+
+
+}  
+
+
+class Main{
+   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    string inputOfString = input.nextString();
+    StringCalculator object = new StringCalculator();
+    object.Add(inputOfString);   
+   }
+}
